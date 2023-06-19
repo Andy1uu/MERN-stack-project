8ae09f7fc0fc37d0fb6ac06e95424456b9075768
Andy Luu <andyluu324@gmail.com>
2023-06-19 13:48:21 -0400

    made uri a string

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
-  "http://localhost:3000"
+  "http://localhost:3000",
+  "https://mern-stack-project-backend.onrender.com",
+  "https://mern-stack-project-frontend.onrender.com"
 ];
 
 module.exports = allowedOrigins;
