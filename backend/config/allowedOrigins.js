dfa59e246d2273224d6e6280b705ee63a87ee667
Andy Luu <andyluu324@gmail.com>
2023-06-19 14:11:07 -0400

    added my site to the origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "https://mern-stack-project-backend.onrender.com",
+  "https://andy1uu.github.io",
   "https://mern-stack-project-frontend.onrender.com"
 ];
 
